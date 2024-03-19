20b6026797f67d63b13e5116490747ecb1338433
Usuario <Usuario@CPD02-M07>
2024-03-19 10:54:44 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("selecione a operação desejada (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            switch (op) 
+            {
+                case "+":
+
+                num1= num1 + num2;
+
+                    Console.WriteLine("resultado: " + num1);
+                break;
+                
+                case "-":
+
+                    num1 = num1 - num2;
+
+                    Console.WriteLine("resultado: " + num1);
+                break;
+
+                case "*":
+
+                    num1 = num1 * num2;
+
+                    Console.WriteLine("resultado: " + num1);
+                break;
+
+                case "/":
+
+                    num1 = num1 / num2;
+
+                    Console.WriteLine("resultado: " + num1);
+                break;
+                
+                default:
+
+                    Console.WriteLine("Opção invalida.");
+                
+                break;
+
+                
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
